9ea391a3a1905675ceda2974e7c2d6c46fb3899b
rayoz12 <rayoz12@yahoo.com.au>
2017-06-01 10:06:55 +1000

    code

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const request = require("request");
+const util = require("util");
+
+const songs = [];
+
+setInterval(makeTrackRequest, 10000);
+
+
+function makeTrackRequest() {
+	request('http://media.arn.com.au/XML-JSON.aspx?source=www.wsfm.com.au&feedUrl=xml/wsfm1017_now.xml', function (error, response, body) {
+		if (error !== null) {
+			console.log("Request error at " + new Date().toLocaleString());
+			return;
+		}
+		console.log("\u001b[2J\u001b[0;0H");
+		console.log("INFO AT: " + new Date().toLocaleString());
+		printSongInfo(JSON.parse(body));
+	});
+}
+
+makeTrackRequest();
+
+function printSongInfo(body) {
+	const data = body.on_air;
+	console.log("-------------Now Playing-------------");
+	const artist = data.now_playing.audio.artist.value;
+	const title = data.now_playing.audio.title.value;
+	const playedAt = data.now_playing.audio.played_datetime.value;
+	console.log("Artist:", artist);
+	console.log("Title:", title);
+	console.log("Played At:", playedAt);
+	console.log("-------------Next Tracks-------------");
+	const nextArr = data.up_next.audio;
+	for (let i=0;i<nextArr.length;i++) {
+		const track = nextArr[i];
+		console.log(`--------------Track ${i+1}-----------`);
+		const nextArtist = track.artist.value;
+		const nextTitle = track.title.value;
+		const nextPlayedAt = track.played_datetime.value;
+		console.log(`Track ${i+1}: Artist:`, nextArtist);
+		console.log(`Track ${i+1}: Title:`, nextTitle);
+		console.log(`Track ${i+1}: Playing At:`, nextPlayedAt);
+		console.log();
+	}
+}
